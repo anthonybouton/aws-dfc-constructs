d4272a9aceaa82811afd00d4f6d9fd73140311af
Anthony <svansay@pansay.com>
2021-07-29 11:37:18 +0200

    add typed definitions for the new buildspec

--- lib/models/index.ts
+++ lib/models/index.ts
@@ -1,4 +1,6 @@
 import { SecretValue } from "aws-cdk-lib";
+import { BuildEnvironmentVariable } from "aws-cdk-lib/lib/aws-codebuild";
+import { IBucket } from "aws-cdk-lib/lib/aws-s3";
 
 export interface ReducedGitHubSourceActionProps {
   /**
@@ -29,3 +31,53 @@ export interface ReducedCodeCommitActionProps {
   readonly branch?: string;
   readonly repoName: string;
 }
+export interface BuildSpec {
+  version: string,
+  phases: {
+    install: {
+      "runtime-versions": { [name: string]: string; }
+    },
+    build: {
+      commands: string[]
+    }
+  },
+  artifacts: {
+    files: string[],
+    "base-directory": string,
+    "secondary-artifacts"?: {
+      [name: string]: {
+        files: string[],
+        "base-directory": string,
+      };
+    }
+  },
+  cache: {
+    paths: string[]
+  }
+}
+export const DEFAULT_ANGULAR_BUILD_SPEC: BuildSpec = {
+  version: "0.2",
+  phases: {
+    install: {
+      "runtime-versions": {
+        nodejs: "12"
+      }
+    },
+    build: {
+      commands: ["npm install", "npm run build"]
+    }
+  },
+  artifacts: {
+    files: ["**/*"],
+    "base-directory": "dist"
+  },
+  cache: {
+    paths: ['/root/.m2/**/*', '/root/.npm/**/*']
+  }
+}
+export interface AngularCodeBuildProps {
+  readonly buildSpec: BuildSpec;
+  readonly buildEnvironmentVariables?: { [name: string]: BuildEnvironmentVariable; };
+  readonly cachingBucket?: IBucket;
+}
+
